fff7422e4c0f74cf73e7a36919d2cc2a75c5c804
trewanek <trewanek@gmail.com>
2020-08-10 23:44:39 +0900

    Initial commit

---
+++ bin/fetch_secrets.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Get secrets list
+secrets=$(gcloud secrets list --format=json | jq -r '.[].name')
+
+# Get secret values
+for path in ${secrets[@]}; do
+	secret_name=$( awk -F'/' '{print $4}' <<< ${path} )
+	if value=$(gcloud secrets versions access latest --secret=${secret_name}); then
+		if [ ! -f .env ]; then
+			touch .env
+			echo '.env file created'
+		fi
+		target=$secret_name; command=/^$target=/d; sed -i '' $command .env
+		echo $secret_name=$value >> .env
+	fi
+done
